4cd6d0176b7c6254cfe31b9fc5177f20d70b9c31
Max Moulds <max@maxmoulds.org>
2016-12-06 19:39:28 -0800

    example of a databes and a;jhgkajhgwe

--- database.js
+++ database.js
@@ -5,7 +5,7 @@ var mongodb = require('mongodb');
 var MongoClient = mongodb.MongoClient;
 
 // Connection URL. This is where your mongodb server is running.
-var url = 'mongodb://localhost:27017/my_database_name';
+var url = 'mongodb://localhost:27017/sheeatsburgers';
 
 // Use connect method to connect to the Server
 MongoClient.connect(url, function (err, db) {
@@ -15,9 +15,23 @@ MongoClient.connect(url, function (err, db) {
     //HURRAY!! We are connected. :)
     console.log('Connection established to', url);
 
-    // do some work here with the database.
+    // Get the documents collection
+    var collection = db.collection('sheeatsburgers');
 
-    //Close connection
-    db.close();
+    //Create some users
+    var user1 = {name: 'modulus admin', age: 42, roles: ['admin', 'moderator', 'user']};
+    var user2 = {name: 'modulus user', age: 22, roles: ['user']};
+    var user3 = {name: 'modulus super admin', age: 92, roles: ['super-admin', 'admin', 'moderator', 'user']};
+
+    // Insert some users
+    collection.insert([user1, user2, user3], function (err, result) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', result.length, result);
+      }
+      //Close connection
+      db.close();
+    });
   }
 });
