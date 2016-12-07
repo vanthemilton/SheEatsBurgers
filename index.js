5ad7f8fc977e5c9ef5821a38cdf9734bf2c5ff8a
Evan Milton <vanthemilton@gmail.com>
2016-12-07 15:51:50 -0800

    js color picker

---
+++ index.js
@@ -0,0 +1,15 @@
+
+//--------------------------------------------------------------------------- Add hover to nav buttons
+
+
+//--------------------------------------------------------------------------- Add Thread
+function addThreadButton(event) {
+	var clickedElem = event.target;
+	if (clickedElem.classList.contains('new-thread-button')) {
+		// show modal for making a thread
+		// get info from thread to create database object
+
+
+
+	}
+}
