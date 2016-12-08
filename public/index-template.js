654e30ed7d01ec531cd77db6ae234574c357a872
Max Moulds <max@maxmoulds.org>
2016-12-07 18:55:28 -0800

    fix the build step of handlebars

--- public/index-template.js
+++ public/index-template.js
@@ -1,6 +1,6 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
 templates['index'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "test\n";
+    return "";
 },"useData":true});
 })();
