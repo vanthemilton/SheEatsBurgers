2ff2573178a63852987fae3c2ea41db8e8633d9c
Evan Milton <vanthemilton@gmail.com>
2016-12-08 15:51:31 -0800

    neat

--- public/index.js
+++ public/index.js
@@ -1,18 +1,36 @@
 
-//--------------------------------------------------------------------------- Event listeners
+//--------------------------------------------------------------------------- rgb to hex converter
+//Function to convert hex format to a rgb color
+function rgb2hex(rgb){
+ rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+ return (rgb && rgb.length === 4) ? "#" +
+  ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
+  ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
+  ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : '';
+}
 
+$('button').click(function(){
+    var hex = rgb2hex( $('input').val() );
+    $('.result').html( hex );
+});
 
 //--------------------------------------------------------------------------- Add Thread
 function addThread() {
 		
-		var title = document.getElementsByName('user');
-		var description = document.getElementsByName('description');
-		var tBun = document.getElementsByName('TBun');
-		/*var patty = document.getElementsByName('description');
+		var title = document.getElementsByName('user')[1].value;
+		var description = document.getElementsByName('description')[1].value;
+
+		var tBunArray = document.getElementsByName('TBun')[0]._jscLinkedInstance.rgb[0];
+		var tBun = rgb2hex(rgba(tBunArray[0], tBunArray[1], tBunArray[2], 1));
+
+		/*
+
+		
+		var patty = document.getElementsByName('description');
 		var BBun = document.getElementsByName('description');
 		var Border = document.getElementsByName('description');*/
 		console.log(title, " ", description, " ", tBun);
-		alert(title[1].value + " " + description[1].value + " " + tBun[0]._jscLinkedInstance.rgb); 
+		//alert(title + " " + description + " " + tBun); 
 };
 
 
